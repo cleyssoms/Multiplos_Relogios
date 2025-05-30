8c874df99b07d322a154818114109cfde51a1f3e
cleyssoms <cleysso.s@edu.pucrs.br>
2025-05-30 13:30:11 -0300

    fix: corrigido sintaxe e deslocamento

--- queue.sv
+++ queue.sv
@@ -1,5 +1,5 @@
 module queue (
-  input  logic        clock10mhz,
+  input  logic        clk_10khz,
   input  logic        reset,
   input  logic [7:0]  data_in,
   input  logic        enqeue_in,
@@ -8,27 +8,34 @@ module queue (
   output logic [7:0]  data_out
 );
 
-logic [63:0] shift_reg;
-logic [7:0] out_data;
-logic [3:0] count;
+  logic [63:0] shift_reg;
+  logic [7:0] data_out_reg;
+  logic [3:0] count = 0;
 
 
-always_ff @(posedge clk_10mhz or posedge reset) begin
+always_ff @(posedge clk_10khz or posedge reset) begin
   if (reset) begin
     count <= 0;
-    shift_reg <= 0; // TODO n sei se precisa ate
-
+    shift_reg <= 64'b0;
+    data_out_reg <= 8'b0;
   end else begin
-    if (enqeue_in && (count < 4'd8)) begin
-      shift_reg <= shift_reg << 8; // TODO rever sintatica
-      shift_reg[7:0] <= data_out;
+    if (enqeue_in && (count < 8)) begin
+      // Insere novo dado nos bits superiores
+      shift_reg <= {data_in, shift_reg[63:8]};
+      count <= count + 1;
     end
 
-    if (dequeue_in && (count > 4'd0)) begin
-      out_data <= shift_reg[(len << 3)]; // multiplica por 8 len(tamanho de cada elemento)
-      len--;
+    else if (dequeue_in && (count > 0)) begin
+      // Captura dado mais antigo
+      data_out_reg <= shift_reg[7:0];
+
+      // Desloca todos os dados para direita
+      shift_reg <= {8'b0, shift_reg[63:8]}
+      count <= count - 1;
     end
+  end
+end
 
-
-assign len_out  = len;
-assign data_out = out_data;
+assign len_out  = count;
+assign data_out = data_out_reg;
+endmodule
